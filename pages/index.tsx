4fd0e3de0d320a1648132b919ee4e2e92cc261e0
abr-ya <gamer05@list.ru>
2023-01-12 20:47:21 +0300

    feature: Create Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+const Home = (): JSX.Element => {
   const [rating, setRating] = useState(0);
 
   return (
@@ -25,4 +26,6 @@ export default function Home(): JSX.Element {
       <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
-}
+};
+
+export default withLayout(Home);
