1cca7df8c974f1814f5203af17533b2fe59c9070
abr-ya <gamer05@list.ru>
2023-01-15 14:44:32 +0300

    feature: Update Routing: add categories

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -8,6 +8,7 @@ export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
     <div className={cn(className, styles.sidebar)} {...props}>
       <Logo className={styles.logo} />
+      <div>поиск</div>
       <Menu />
     </div>
   );
